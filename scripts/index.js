0a6b483ca38dec67d0b868803a541db0c76c0945
Sergey Tsepilov <stkz94@mail.ru>
2023-01-06 14:38:07 +0600

    index

--- scripts/index.js
+++ scripts/index.js
@@ -1,3 +1,33 @@
 const date = new Date();
 const year = date.getFullYear();
 document.getElementById("currentYear").innerHTML = year;
+
+let profileEditButton = document.querySelector(".profile__edit-btn");
+let popup = document.querySelector(".popup");
+let popupCloseButton = document.querySelector(".popup__close-btn");
+let formElement = document.querySelector(".popup__form");
+let profileTitle = document.querySelector(".profile__title");
+let profileSubtitle = document.querySelector(".profile__subtitle");
+let nameInput = document.querySelector(".popup__input_type_name");
+let descriptionInput = document.querySelector(".popup__input_type_description");
+
+function openPopup() {
+  popup.classList.add("popup_opened");
+  nameInput.value = profileTitle.textContent;
+  descriptionInput.value = profileSubtitle.textContent;
+}
+
+function closePopup() {
+  popup.classList.remove("popup_opened");
+}
+
+function formSubmitHandler(evt) {
+  evt.preventDefault();
+  profileTitle.textContent = nameInput.value;
+  profileSubtitle.textContent = descriptionInput.value;
+  closePopup();
+}
+
+profileEditButton.addEventListener("click", openPopup);
+popupCloseButton.addEventListener("click", closePopup);
+formElement.addEventListener("submit", formSubmitHandler);
